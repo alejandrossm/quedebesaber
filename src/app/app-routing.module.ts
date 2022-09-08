262134d5a1f90a77c68698a98322cb8d2073f5b8
Alejandro <al.sepulvedam@profesor.duoc.cl>
2022-09-08 13:31:29 -0300

    con animaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,32 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'loginpage',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+
+  {
+    path: 'loginpage',
+    loadChildren: () => import('./pages/loginpage/loginpage.module').then( m => m.LoginpagePageModule)
+  },
+  {
+    path: 'cards',
+    loadChildren: () => import('./pages/cards/cards.module').then( m => m.CardsPageModule)
+  },
+  {
+    path: 'listas',
+    loadChildren: () => import('./pages/listas/listas.module').then( m => m.ListasPageModule)
+  },
+  {
+    path: 'refresher',
+    loadChildren: () => import('./pages/refresher/refresher.module').then( m => m.RefresherPageModule)
   },
 ];
 
